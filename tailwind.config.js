c5ca11188429b42843d78fb62703fb1c019a1c9d
HelenEBates <helen@brevio.org>
2024-03-26 12:58:13 +0000

    add tailwind and config, plus some styling, the custom colors need fixing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,15 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    colors: {
+      'opal': '#EEEAE2',
+      'vermilion': '#EA3125',
+      'slate': '#797572',
+      'midnight': '#0F0C07',
+      'rose': '#EFB7A7',
+      'dark__rose': '#E4847E'
+    }
+  }
 }
